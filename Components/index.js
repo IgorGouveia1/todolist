096b19d143f136429db47c1fc32bb85a62a274be
IgorGouveia1 <igor.roichman@gmail.com>
2022-04-01 15:50:14 -0300

    create js const

---
+++ Components/index.js
@@ -0,0 +1,15 @@
+const txtInput = document.querySelector('.txt-input')
+
+const btnInput = document.querySelector('.btn-input')
+
+const listApp = document.querySelector('.list-app')
+
+
+
+btnInput.addEventListener('click', addTask)
+
+function addTask () {
+    event.preventDefault()
+
+    
+}
