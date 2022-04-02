2a63584bc54c9d3defd5f3ea96a0419e98f7b3bf
IgorGouveia1 <igor.roichman@gmail.com>
2022-04-02 11:36:38 -0300

    adding task, starting done task

--- Components/index.js
+++ Components/index.js
@@ -11,5 +11,27 @@ btnInput.addEventListener('click', addTask)
 function addTask () {
     event.preventDefault()
 
+    const listContainter = document.createElement('div')
+    listContainter.classList.add('list-container')
+
+    const addList = document.createElement('li')
+    listContainter.classList.add('add-List')
+    addList.innerText = txtInput.value 
+    txtInput.value = ''
+
+    listContainter.appendChild(addList)
+
+    const doneBtn = document.createElement('button')
+    doneBtn.innerHTML = '<span class="material-icons-outlined">done</span>'
+    listContainter.classList.add('done-btn')
+    doneBtn.onclick = addList.classList.add('complete-task')
     
+    listContainter.appendChild(doneBtn)
+
+    listApp.appendChild(listContainter)
+
 }
+
+
+
+
